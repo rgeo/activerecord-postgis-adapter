d37649a7f53ed7eac2f34d759b33f5f5a32b61f3
Daniel Azuma <dazuma@gmail.com>
2013-02-04 13:09:59 -0800

    Fix compatibility issue with a driver loading change in jdbc-postgres 9.2.1002.1

--- lib/active_record/connection_adapters/postgis_adapter/jdbc_connection.rb
+++ lib/active_record/connection_adapters/postgis_adapter/jdbc_connection.rb
@@ -47,23 +47,32 @@ class ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
 end
 
 class ::ActiveRecord::Base
-  # ActiveRecord looks for the postgis_connection factory method in
-  # this class.
-  #
-  # Based on the default `postgresql_connection` definition from
-  # activerecord-jdbc-adapter's:
-  # lib/arjdbc/postgresql/connection_methods.rb
-  def self.postgis_connection(config)
-    require "arjdbc/postgresql"
-    config[:host] ||= "localhost"
-    config[:port] ||= 5432
-    config[:url] ||= "jdbc:postgresql://#{config[:host]}:#{config[:port]}/#{config[:database]}"
-    config[:url] << config[:pg_params] if config[:pg_params]
-    config[:driver] ||= "org.postgresql.Driver"
-    config[:adapter_class] = ::ActiveRecord::ConnectionAdapters::PostGISAdapter::MainAdapter
-    config[:adapter_spec] = ::ArJdbc::PostgreSQL
-    conn = jdbc_connection(config)
-    conn.execute("SET SEARCH_PATH TO #{config[:schema_search_path]}") if config[:schema_search_path]
-    conn
+  class << self
+    # ActiveRecord looks for the postgis_connection factory method in
+    # this class.
+    #
+    # Based on the default `postgresql_connection` definition from
+    # activerecord-jdbc-adapter's:
+    # lib/arjdbc/postgresql/connection_methods.rb
+    def postgis_connection(config)
+      begin
+        require 'jdbc/postgres'
+        ::Jdbc::Postgres.load_driver(:require) if defined?(::Jdbc::Postgres.load_driver)
+      rescue LoadError # assuming driver.jar is on the class-path
+      end
+      require "arjdbc/postgresql"
+      config[:username] ||= ::Java::JavaLang::System.get_property("user.name")
+      config[:host] ||= "localhost"
+      config[:port] ||= 5432
+      config[:url] ||= "jdbc:postgresql://#{config[:host]}:#{config[:port]}/#{config[:database]}"
+      config[:url] << config[:pg_params] if config[:pg_params]
+      config[:driver] ||= defined?(::Jdbc::Postgres.driver_name) ? ::Jdbc::Postgres.driver_name : 'org.postgresql.Driver'
+      config[:adapter_class] = ::ActiveRecord::ConnectionAdapters::PostGISAdapter::MainAdapter
+      config[:adapter_spec] = ::ArJdbc::PostgreSQL
+      conn = jdbc_connection(config)
+      conn.execute("SET SEARCH_PATH TO #{config[:schema_search_path]}") if config[:schema_search_path]
+      conn
+    end
+    alias_method :jdbcpostgis_connection, :postgis_connection
   end
 end
