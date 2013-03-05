48d818d3b861cff2fdf8b9d4949ac04d90f8c021
Nick Muerdter <nick.muerdter@nrel.gov>
2013-03-05 11:31:19 -0700

    Fix database setup rake tasks under jruby/jdbc adapter.
    ActiveRecord's quote_column_name is equivalent to pg's quote_ident.
    Under activerecord's default adapter it actually just calls quote_ident,
    while under the jdbc adpater it provides a ruby-based implementation.

--- lib/active_record/connection_adapters/postgis_adapter/rails4/postgis_database_tasks.rb
+++ lib/active_record/connection_adapters/postgis_adapter/rails4/postgis_database_tasks.rb
@@ -113,7 +113,7 @@ module ActiveRecord  # :nodoc:
         end
 
         def quoted_username
-          @quoted_username ||= ::PGconn.quote_ident(username)
+          @quoted_username ||= ::ActiveRecord::Base.connection.quote_column_name(username)
         end
 
         def password
