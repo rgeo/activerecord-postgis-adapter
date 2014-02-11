9b704b520673c46254c1112d77cf89743afa694c
Tee Parham <tee@neighborland.com>
2014-02-11 13:01:32 -0700

    Remove AR version checks

--- lib/active_record/connection_adapters/postgis_adapter/shared/railtie.rb
+++ lib/active_record/connection_adapters/postgis_adapter/shared/railtie.rb
@@ -1,30 +1,13 @@
 unless defined?(::ActiveRecord::ConnectionAdapters::PostGISAdapter::Railtie)
-
   module ActiveRecord  # :nodoc:
-
     module ConnectionAdapters  # :nodoc:
-
       module PostGISAdapter  # :nodoc:
-
-
         class Railtie < ::Rails::Railtie  # :nodoc:
-
           rake_tasks do
-            directory_ = case ::ActiveRecord::VERSION::MAJOR
-              when 3 then 'rails3'
-              when 4 then 'rails4'
-              else raise "Unsupported ActiveRecord version #{::ActiveRecord::VERSION::STRING}"
-            end
-            load ::File.expand_path("../#{directory_}/databases.rake", ::File.dirname(__FILE__))
+            load ::File.expand_path("../rails4/databases.rake", ::File.dirname(__FILE__))
           end
-
         end
-
-
       end
-
     end
-
   end
-
 end
