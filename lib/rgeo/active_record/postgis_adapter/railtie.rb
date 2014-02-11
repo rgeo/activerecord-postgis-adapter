ccf88e6052f33de2feb8249b8d6d7f484a81c200
Tee Parham <tee@neighborland.com>
2014-02-11 12:53:33 -0700

    Remove unused railtie file

--- lib/rgeo/active_record/postgis_adapter/railtie.rb
+++
@@ -1,3 +0,0 @@
-puts 'WARNING: requiring "rgeo/active_record/postgis_adapter/railtie" is deprecated. Generally, the normal Bundle.require will do the trick. If you need to require the railtie explicitly, require "active_record/connection_adapters/postgis_adapter/railtie"'
-
-require 'active_record/connection_adapters/postgis_adapter/railtie'
