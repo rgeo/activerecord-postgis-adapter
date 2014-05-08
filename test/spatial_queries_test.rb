72175c9c5fec75fac0b481159687e732cc516deb
Tee Parham <tee@neighborland.com>
2014-05-07 20:56:09 -0600

    Remove test check for spatial_expressions_supported?
    This check was only necessary before Arel 2.1

--- test/spatial_queries_test.rb
+++ test/spatial_queries_test.rb
@@ -59,50 +59,45 @@ module RGeo
               assert_nil(obj3_)
             end
 
-            if ::RGeo::ActiveRecord.spatial_expressions_supported?
-
-              def test_query_st_distance
-                klass_ = populate_ar_class(:mercator_point)
-                obj_ = klass_.new
-                obj_.latlon = @factory.point(1.0, 2.0)
-                obj_.save!
-                id_ = obj_.id
-                obj2_ = klass_.where(klass_.arel_table[:latlon].st_distance('SRID=3785;POINT(2 3)').lt(2)).first
-                refute_nil(obj2_)
-                assert_equal(id_, obj2_.id)
-                obj3_ = klass_.where(klass_.arel_table[:latlon].st_distance('SRID=3785;POINT(2 3)').gt(2)).first
-                assert_nil(obj3_)
-              end
-
-              def test_query_st_distance_from_constant
-                klass_ = populate_ar_class(:mercator_point)
-                obj_ = klass_.new
-                obj_.latlon = @factory.point(1.0, 2.0)
-                obj_.save!
-                id_ = obj_.id
-                obj2_ = klass_.where(::Arel.spatial('SRID=3785;POINT(2 3)').st_distance(klass_.arel_table[:latlon]).lt(2)).first
-                refute_nil(obj2_)
-                assert_equal(id_, obj2_.id)
-                obj3_ = klass_.where(::Arel.spatial('SRID=3785;POINT(2 3)').st_distance(klass_.arel_table[:latlon]).gt(2)).first
-                assert_nil(obj3_)
-              end
-
-              def test_query_st_length
-                klass_ = populate_ar_class(:path_linestring)
-                obj_ = klass_.new
-                obj_.path = @factory.line(@factory.point(1.0, 2.0), @factory.point(3.0, 2.0))
-                obj_.save!
-                id_ = obj_.id
-                obj2_ = klass_.where(klass_.arel_table[:path].st_length.eq(2)).first
-                refute_nil(obj2_)
-                assert_equal(id_, obj2_.id)
-                obj3_ = klass_.where(klass_.arel_table[:path].st_length.gt(3)).first
-                assert_nil(obj3_)
-              end
-
-            else
-              puts "WARNING: The current Arel does not support named functions. Spatial expression tests skipped."
+            def test_query_st_distance
+              klass_ = populate_ar_class(:mercator_point)
+              obj_ = klass_.new
+              obj_.latlon = @factory.point(1.0, 2.0)
+              obj_.save!
+              id_ = obj_.id
+              obj2_ = klass_.where(klass_.arel_table[:latlon].st_distance('SRID=3785;POINT(2 3)').lt(2)).first
+              refute_nil(obj2_)
+              assert_equal(id_, obj2_.id)
+              obj3_ = klass_.where(klass_.arel_table[:latlon].st_distance('SRID=3785;POINT(2 3)').gt(2)).first
+              assert_nil(obj3_)
             end
+
+            def test_query_st_distance_from_constant
+              klass_ = populate_ar_class(:mercator_point)
+              obj_ = klass_.new
+              obj_.latlon = @factory.point(1.0, 2.0)
+              obj_.save!
+              id_ = obj_.id
+              obj2_ = klass_.where(::Arel.spatial('SRID=3785;POINT(2 3)').st_distance(klass_.arel_table[:latlon]).lt(2)).first
+              refute_nil(obj2_)
+              assert_equal(id_, obj2_.id)
+              obj3_ = klass_.where(::Arel.spatial('SRID=3785;POINT(2 3)').st_distance(klass_.arel_table[:latlon]).gt(2)).first
+              assert_nil(obj3_)
+            end
+
+            def test_query_st_length
+              klass_ = populate_ar_class(:path_linestring)
+              obj_ = klass_.new
+              obj_.path = @factory.line(@factory.point(1.0, 2.0), @factory.point(3.0, 2.0))
+              obj_.save!
+              id_ = obj_.id
+              obj2_ = klass_.where(klass_.arel_table[:path].st_length.eq(2)).first
+              refute_nil(obj2_)
+              assert_equal(id_, obj2_.id)
+              obj3_ = klass_.where(klass_.arel_table[:path].st_length.gt(3)).first
+              assert_nil(obj3_)
+            end
+
           end
         end
       end
