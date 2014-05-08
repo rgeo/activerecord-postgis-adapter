622fd548cf0d0603dc213c93a06cd52e258536ff
Tee Parham <tee@neighborland.com>
2014-05-07 21:23:53 -0600

    Match code from rails

--- lib/active_record/connection_adapters/postgis_adapter/rails4/create_connection.rb
+++ lib/active_record/connection_adapters/postgis_adapter/rails4/create_connection.rb
@@ -1,29 +1,27 @@
+require 'pg'
+
 module ActiveRecord  # :nodoc:
-
   module ConnectionHandling  # :nodoc:
-    require 'pg'
 
-    # Based on the default <tt>postgresql_connection</tt> definition from
-    # ActiveRecord.
-
-    def postgis_connection(config_)
+    # Based on the default <tt>postgresql_connection</tt> definition from ActiveRecord.
+    # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+    def postgis_connection(config)
       # FULL REPLACEMENT because we need to create a different class.
-      conn_params_ = config_.symbolize_keys
+      conn_params = config.symbolize_keys
 
-      conn_params_.delete_if { |_, v_| v_.nil? }
+      conn_params.delete_if { |_, v| v.nil? }
 
       # Map ActiveRecords param names to PGs.
-      conn_params_[:user] = conn_params_.delete(:username) if conn_params_[:username]
-      conn_params_[:dbname] = conn_params_.delete(:database) if conn_params_[:database]
+      conn_params[:user] = conn_params.delete(:username) if conn_params[:username]
+      conn_params[:dbname] = conn_params.delete(:database) if conn_params[:database]
 
       # Forward only valid config params to PGconn.connect.
-      conn_params_.keep_if { |k_, _| VALID_CONN_PARAMS.include?(k_) }
+      conn_params.keep_if { |k, _| VALID_CONN_PARAMS.include?(k) }
 
       # The postgres drivers don't allow the creation of an unconnected PGconn object,
       # so just pass a nil connection object for the time being.
-      ::ActiveRecord::ConnectionAdapters::PostGISAdapter::MainAdapter.new(nil, logger, conn_params_, config_)
+      ::ActiveRecord::ConnectionAdapters::PostGISAdapter::MainAdapter.new(nil, logger, conn_params, config)
     end
 
   end
-
 end
