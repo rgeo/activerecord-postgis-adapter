e27791477520af91655a56588d0f89a19a19c5f1
Tee Parham <tee@neighborland.com>
2014-05-07 21:08:34 -0600

    Use simple array merge to add ignore_tables

--- lib/active_record/connection_adapters/postgis_adapter/shared/setup.rb
+++ lib/active_record/connection_adapters/postgis_adapter/shared/setup.rb
@@ -1,21 +1,11 @@
 module ActiveRecord  # :nodoc:
-
   module ConnectionAdapters  # :nodoc:
-
     module PostGISAdapter  # :nodoc:
 
-
       def self.initial_setup
-        gis_ignore_tables_ = ['geometry_columns', 'spatial_ref_sys', 'layer', 'topology']
-        ignore_tables_ = ::ActiveRecord::SchemaDumper.ignore_tables
-        gis_ignore_tables_.each do |table_|
-          ignore_tables_ << table_ unless ignore_tables_.include?(table_)
-        end
+        ::ActiveRecord::SchemaDumper.ignore_tables |= %w[geometry_columns spatial_ref_sys layer topology]
       end
 
-
     end
-
   end
-
 end
