5b5f201cf8337370ceaac23d975886ba85b482a0
Tee Parham <tee@neighborland.com>
2014-05-11 13:52:39 -0600

    Add byebug in development

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gemspec
 
 gem 'pg', '~> 0.17', platform: :ruby
+gem 'byebug', platform: :ruby
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 require 'minitest/autorun'
 require 'rgeo/active_record/adapter_test_helper'
 
+begin
+  require 'byebug'
+rescue LoadError
+  # ignore
+end
+
 BASE_TEST_CLASS = if ActiveRecord::VERSION::STRING > '4.1'
                     Minitest::Test
                   else
