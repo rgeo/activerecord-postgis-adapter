ed1a6b9404c1da5ecafaec353912610d0d57b8ae
Tee Parham <tee@neighborland.com>
2014-05-15 09:38:48 -0600

    Remove unnecessary defined? wrapper around Railtie

--- lib/active_record/connection_adapters/postgis_adapter/railtie.rb
+++ lib/active_record/connection_adapters/postgis_adapter/railtie.rb
@@ -1,17 +1,14 @@
 require 'rails/railtie'
 require 'active_record/connection_adapters/postgis_adapter'
 
-unless defined?(::ActiveRecord::ConnectionAdapters::PostGISAdapter::Railtie)
-  module ActiveRecord  # :nodoc:
-    module ConnectionAdapters  # :nodoc:
-      module PostGISAdapter  # :nodoc:
-        class Railtie < ::Rails::Railtie  # :nodoc:
-          rake_tasks do
-            load ::File.expand_path("databases.rake", ::File.dirname(__FILE__))
-          end
+module ActiveRecord  # :nodoc:
+  module ConnectionAdapters  # :nodoc:
+    module PostGISAdapter  # :nodoc:
+      class Railtie < ::Rails::Railtie  # :nodoc:
+        rake_tasks do
+          load ::File.expand_path("databases.rake", ::File.dirname(__FILE__))
         end
       end
     end
   end
 end
-
