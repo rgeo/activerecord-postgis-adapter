f6d0b6a81529c00db80532ca945bc653742d21dc
Phlegx Systems OG <register@phlegx.com>
2014-05-28 22:35:21 +0200

    Bug fix in search path tiger_data.

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -7,7 +7,7 @@ module RGeo
         class SpatialQueriesTest < ActiveSupport::TestCase  # :nodoc:
 
           def test_ignore_tables
-            assert_equal %w(geometry_columns spatial_ref_sys layer topology tiger), ::ActiveRecord::SchemaDumper.ignore_tables
+            assert_equal %w(geometry_columns spatial_ref_sys layer topology tiger tiger_data), ::ActiveRecord::SchemaDumper.ignore_tables
           end
 
         end
