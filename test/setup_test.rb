cd873439a1d6ff5409218310b047e56f50e5ff8a
Phlegx Systems OG <register@phlegx.com>
2014-05-28 21:01:18 +0200

    Add postgis_tiger_geocoder support.

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -7,7 +7,7 @@ module RGeo
         class SpatialQueriesTest < ActiveSupport::TestCase  # :nodoc:
 
           def test_ignore_tables
-            assert_equal %w(geometry_columns spatial_ref_sys layer topology), ::ActiveRecord::SchemaDumper.ignore_tables
+            assert_equal %w(geometry_columns spatial_ref_sys layer topology tiger), ::ActiveRecord::SchemaDumper.ignore_tables
           end
 
         end
