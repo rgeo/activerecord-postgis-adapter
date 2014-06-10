471690ebb57de462332302c5f67e351da280df71
Tee Parham <tee@neighborland.com>
2014-06-10 10:11:59 -0600

    Test spatial column info caching

--- test/ddl_test.rb
+++ test/ddl_test.rb
@@ -225,14 +225,27 @@ module RGeo
               assert_equal(0, klass.connection.select_value(geometry_column_count_query).to_i)
             end
 
-            def test_no_geography_sql_query
+            def test_caches_spatial_column_info
+              klass = create_ar_class
+              klass.connection.create_table(:spatial_test, force: true) do |t|
+                t.point 'latlon'
+                t.point 'other'
+              end
+              ::ActiveRecord::ConnectionAdapters::PostGISAdapter::SpatialColumnInfo.any_instance.expects(:all).once.returns({})
+              klass.columns
+              klass.columns
+            end
+
+            def test_no_query_spatial_column_info
               klass = create_ar_class
               klass.connection.create_table(:spatial_test, force: true) do |t|
                 t.string 'name'
               end
-              # `all` does the database query - it should not be called when klass.columns is called
+              # `all` queries column info from the database - it should not be called when klass.columns is called
               ::ActiveRecord::ConnectionAdapters::PostGISAdapter::SpatialColumnInfo.any_instance.expects(:all).never
-              refute klass.columns.first.spatial?
+              # first column is id, second is name
+              refute klass.columns[1].spatial?
+              assert_nil klass.columns[1].has_z
             end
           end
 
