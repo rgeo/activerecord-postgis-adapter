70b0e48906b9d1f8370ca3c72a7381da577be2a3
Tee Parham <tee@neighborland.com>
2014-11-07 10:33:52 -0700

    [WIP] Add #add_spatial_column to SchemaStatements

--- lib/active_record/connection_adapters/postgis_adapter/schema_statements.rb
+++ lib/active_record/connection_adapters/postgis_adapter/schema_statements.rb
@@ -16,6 +16,24 @@ module ActiveRecord
           end
         end
 
+        def add_spatial_column(o)
+          # info = options[:info] || {}
+          # options.merge!(o.limit) if o.limit.is_a?(::Hash)
+          type = o.type.to_s.gsub('_', '').upcase
+          # srid = (options[:srid] || PostGISAdapter::DEFAULT_SRID).to_i
+          if o.geographic?
+            type << 'Z' if o.has_z?
+            type << 'M' if o.has_m?
+            "ADD COLUMN #{quote_column_name(o.name)} GEOGRAPHY(#{o.type},#{o.srid})"
+          else
+            raise NotImplementedError
+            # type = "#{type}M" if o.has_m? && !o.has_z?
+            # dimensions = set_dimensions(has_m, has_z)
+            # execute("SELECT AddGeometryColumn('#{quote_string(table_name)}', '#{quote_string(column_name)}', #{o.srid}, '#{quote_string(o.type)}', #{dimensions})")
+            # change_column_null(table_name, column_name, false, options[:default]) if options[:null] == false
+          end
+        end
+
       end
     end
   end
