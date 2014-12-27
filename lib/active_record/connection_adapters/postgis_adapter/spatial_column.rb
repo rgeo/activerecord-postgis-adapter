5a0959a2ded8b89a4d1875f1f575cd475f1d50b7
Tee Parham <tee@neighborland.com>
2014-12-26 17:49:17 -0700

    Comment debug raise

--- lib/active_record/connection_adapters/postgis_adapter/spatial_column.rb
+++ lib/active_record/connection_adapters/postgis_adapter/spatial_column.rb
@@ -102,8 +102,8 @@ module ActiveRecord  # :nodoc:
 
             # TODO - only valid types should be allowed
             # e.g. linestring is not valid for point column
+            # raise "maybe should raise" unless RGeo::Feature::Geometry.check_type(geo_value)
 
-            raise "maybe should raise" unless RGeo::Feature::Geometry.check_type(geo_value)
             RGeo::WKRep::WKBGenerator.new(hex_format: true, type_format: :ewkb, emit_ewkb_srid: true)
               .generate(geo_value)
           end
