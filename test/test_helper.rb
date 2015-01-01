a8e114892919c257b20d6cd2f1632d6661bedb08
Tee Parham <tee@neighborland.com>
2015-01-01 15:57:28 -0700

    Less ::

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,18 +12,18 @@ end
 class ActiveSupport::TestCase
   self.test_order = :random
 
-  DATABASE_CONFIG_PATH = ::File.dirname(__FILE__) << '/database.yml'
+  DATABASE_CONFIG_PATH = File.dirname(__FILE__) << '/database.yml'
 
-  class SpatialModel < ::ActiveRecord::Base
-    establish_connection ::YAML.load_file(DATABASE_CONFIG_PATH)
+  class SpatialModel < ActiveRecord::Base
+    establish_connection YAML.load_file(DATABASE_CONFIG_PATH)
   end
 
   def factory
-    ::RGeo::Cartesian.preferred_factory(srid: 3785)
+    RGeo::Cartesian.preferred_factory(srid: 3785)
   end
 
   def geographic_factory
-    ::RGeo::Geographic.spherical_factory(srid: 4326)
+    RGeo::Geographic.spherical_factory(srid: 4326)
   end
 
 end
