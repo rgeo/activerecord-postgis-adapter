e4e3442e928af0578156bb43922dc81060ae6127
Tee Parham <tee@neighborland.com>
2015-02-08 16:53:58 -0700

    Simplify NestedClassTest
    * Remove dependency on RGeo::ActiveRecord::AdapterTestHelper

--- test/nested_class_test.rb
+++ test/nested_class_test.rb
@@ -1,31 +1,21 @@
 require 'test_helper'
 
 class NestedClassTest < ActiveSupport::TestCase  # :nodoc:
-  DATABASE_CONFIG_PATH = ::File.dirname(__FILE__) + '/database.yml'
-
-  include RGeo::ActiveRecord::AdapterTestHelper
-
   module Foo
     def self.table_name_prefix
       'foo_'
     end
-    class Bar < ::ActiveRecord::Base
+    class Bar < ActiveRecord::Base
+      establish_connection YAML.load_file(ActiveSupport::TestCase::DATABASE_CONFIG_PATH)
     end
   end
 
-  define_test_methods do
-
-    def test_nested_model
-      Foo::Bar.class_eval do
-        establish_connection(DATABASE_CONFIG)
-      end
-      Foo::Bar.connection.create_table(:foo_bars, force: true) do |t|
-        t.column 'latlon', :st_point, srid: 3785
-      end
-      Foo::Bar.all
-      Foo::Bar.connection.drop_table(:foo_bars)
+  def test_nested_model
+    Foo::Bar.connection.create_table(:foo_bars, force: true) do |t|
+      t.column 'latlon', :st_point, srid: 3785
     end
-
+    assert_empty Foo::Bar.all
+    assert_equal 1, Foo::Bar.connection.drop_table(:foo_bars).result_status
   end
 
 end
