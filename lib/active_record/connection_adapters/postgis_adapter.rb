d6278f9ddf1dda2358dc3ac908aafac2109fdf0f
Tee Parham <tee@neighborland.com>
2015-04-26 15:28:30 -0600

    Require rgeo-activerecord
    Fix #177
    
    Partially reverts 2804545e5

--- lib/active_record/connection_adapters/postgis_adapter.rb
+++ lib/active_record/connection_adapters/postgis_adapter.rb
@@ -4,6 +4,7 @@
 # :stopdoc:
 
 require 'active_record/connection_adapters/postgresql_adapter'
+require 'rgeo/active_record'
 require 'active_record/connection_adapters/postgis_adapter/version'
 require 'active_record/connection_adapters/postgis_adapter/schema_statements'
 require 'active_record/connection_adapters/postgis_adapter/main_adapter'
@@ -15,6 +16,7 @@ require 'active_record/connection_adapters/postgis_adapter/setup'
 require 'active_record/connection_adapters/postgis_adapter/oid/spatial'
 require 'active_record/connection_adapters/postgis_adapter/create_connection'
 require 'active_record/connection_adapters/postgis_adapter/postgis_database_tasks'
+
 
 ::ActiveRecord::ConnectionAdapters::PostGISAdapter.initial_setup
 
