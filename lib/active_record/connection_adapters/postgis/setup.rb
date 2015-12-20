951fa37c127b53359a351653c844ab937cbc939c
Tee Parham <tee@neighborland.com>
2015-12-20 14:51:06 -0700

    [style] Delimiters

--- lib/active_record/connection_adapters/postgis/setup.rb
+++ lib/active_record/connection_adapters/postgis/setup.rb
@@ -2,7 +2,7 @@ module ActiveRecord  # :nodoc:
   module ConnectionAdapters  # :nodoc:
     module PostGIS  # :nodoc:
       def self.initial_setup
-        ::ActiveRecord::SchemaDumper.ignore_tables |= %w[geometry_columns spatial_ref_sys layer topology]
+        ::ActiveRecord::SchemaDumper.ignore_tables |= %w(geometry_columns spatial_ref_sys layer topology)
       end
     end
   end
