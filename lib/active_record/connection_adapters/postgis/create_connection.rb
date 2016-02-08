44347815aeffe644053103d503ca416e4373c3ef
Tee Parham <tee@neighborland.com>
2016-02-08 09:56:12 -0700

    Update connection based on upstream #postgresql_connection

--- lib/active_record/connection_adapters/postgis/create_connection.rb
+++ lib/active_record/connection_adapters/postgis/create_connection.rb
@@ -19,8 +19,8 @@ module ActiveRecord  # :nodoc:
 
       # Based on the default <tt>postgresql_connection</tt> definition from ActiveRecord.
       # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+      # FULL REPLACEMENT because we need to create a different class.
       def postgis_connection(config)
-        # FULL REPLACEMENT because we need to create a different class.
         conn_params = config.symbolize_keys
 
         conn_params.delete_if { |_, v| v.nil? }
@@ -30,7 +30,8 @@ module ActiveRecord  # :nodoc:
         conn_params[:dbname] = conn_params.delete(:database) if conn_params[:database]
 
         # Forward only valid config params to PGconn.connect.
-        conn_params.keep_if { |k, _| VALID_CONN_PARAMS.include?(k) }
+        valid_conn_param_keys = PGconn.conndefaults_hash.keys + [:requiressl]
+        conn_params.slice!(*valid_conn_param_keys)
 
         # The postgres drivers don't allow the creation of an unconnected PGconn object,
         # so just pass a nil connection object for the time being.
