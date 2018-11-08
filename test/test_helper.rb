70577137d636da4a2600979048e8c9f46032c5c2
Tee Parham <tee@neighborland.com>
2018-11-08 15:42:23 -0700

    Improve spatial queries tests
    * Ignore multipoint query for PG10 (for now)
    * Add helpers to query database version

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,24 +12,6 @@ rescue LoadError
   # ignore
 end
 
-module ActiveSupport
-  class TestCase
-    self.test_order = :sorted
-
-    def factory
-      RGeo::Cartesian.preferred_factory(srid: 3785)
-    end
-
-    def geographic_factory
-      RGeo::Geographic.spherical_factory(srid: 4326)
-    end
-
-    def spatial_factory_store
-      RGeo::ActiveRecord::SpatialFactoryStore.instance
-    end
-  end
-end
-
 module ActiveRecord
   class Base
     DATABASE_CONFIG_PATH = File.dirname(__FILE__) << "/database.yml"
@@ -46,4 +28,30 @@ end
 
 class SpatialModel < ActiveRecord::Base
   establish_test_connection
+end
+
+module ActiveSupport
+  class TestCase
+    self.test_order = :sorted
+
+    def database_version
+      @database_version ||= SpatialModel.connection.select_value("SELECT version()")
+    end
+
+    def pg_10?
+      database_version.include?("PostgreSQL 10")
+    end
+
+    def factory
+      RGeo::Cartesian.preferred_factory(srid: 3785)
+    end
+
+    def geographic_factory
+      RGeo::Geographic.spherical_factory(srid: 4326)
+    end
+
+    def spatial_factory_store
+      RGeo::ActiveRecord::SpatialFactoryStore.instance
+    end
+  end
 end
