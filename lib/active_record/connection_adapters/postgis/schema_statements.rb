cb7001f7c4b47f7ff8a90a717135836e93fec4b8
Tee Parham <tee@neighborland.com>
2019-08-20 09:35:45 -0600

    Remove overrides (temporary commit)

--- lib/active_record/connection_adapters/postgis/schema_statements.rb
+++ lib/active_record/connection_adapters/postgis/schema_statements.rb
@@ -5,45 +5,6 @@ module ActiveRecord
     module PostGIS
       module SchemaStatements
         # override
-        # pass table_name to #new_column
-        def columns(table_name)
-          # Limit, precision, and scale are all handled by the superclass.
-          column_definitions(table_name).map do |column_name, type, default, notnull, oid, fmod, collation, comment|
-            oid = oid.to_i
-            fmod = fmod.to_i
-            type_metadata = fetch_type_metadata(column_name, type, oid, fmod)
-            cast_type = get_oid_type(oid.to_i, fmod.to_i, column_name, type)
-            default_value = extract_value_from_default(default)
-
-            default_function = extract_default_function(default_value, default)
-            new_column(table_name, column_name, default_value, cast_type, type_metadata, !notnull,
-                       default_function, collation, comment)
-          end
-        end
-
-        # override
-        def new_column(table_name, column_name, default, cast_type, sql_type_metadata = nil,
-                       null = true, default_function = nil, collation = nil, comment = nil)
-          # JDBC gets true/false in Rails 4, where other platforms get 't'/'f' strings.
-          if null.is_a?(String)
-            null = (null == "t")
-          end
-
-          column_info = spatial_column_info(table_name).get(column_name, sql_type_metadata.sql_type)
-
-          SpatialColumn.new(column_name,
-                            default,
-                            sql_type_metadata,
-                            null,
-                            table_name,
-                            default_function,
-                            collation,
-                            comment,
-                            cast_type,
-                            column_info)
-        end
-
-        # override
         # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/connection_adapters/postgresql/schema_statements.rb#L583
         #
         # returns Postgresql sql type string
