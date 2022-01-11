a1b7ba1847bee5411fc8757c6f899ae4dfe9d45d
Denis Diachkov <d.diachkov@gmail.com>
2022-01-11 10:40:52 -0800

    Fix code reference comment

--- lib/active_record/connection_adapters/postgis/schema_statements.rb
+++ lib/active_record/connection_adapters/postgis/schema_statements.rb
@@ -5,7 +5,7 @@ module ActiveRecord
     module PostGIS
       module SchemaStatements
         # override
-        # https://github.com/rails/rails/blob/6-0-stable/activerecord/lib/active_record/connection_adapters/postgresql/schema_statements.rb#L624
+        # https://github.com/rails/rails/blob/7-0-stable/activerecord/lib/active_record/connection_adapters/postgresql/schema_statements.rb#L662
         # Create a SpatialColumn instead of a PostgreSQL::Column
         def new_column_from_field(table_name, field)
           column_name, type, default, notnull, oid, fmod, collation, comment, attgenerated = field
