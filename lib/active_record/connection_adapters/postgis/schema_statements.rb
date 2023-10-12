7778ebebb14c6ed6dbc293fcb71da8b14ca09187
Jose Ramon C <joserracamacho@gmail.com>
2023-10-11 23:41:21 -0600

    restore unnecessary change

--- lib/active_record/connection_adapters/postgis/schema_statements.rb
+++ lib/active_record/connection_adapters/postgis/schema_statements.rb
@@ -12,11 +12,11 @@ module ActiveRecord
           type_metadata = fetch_type_metadata(column_name, type, oid.to_i, fmod.to_i)
           default_value = extract_value_from_default(default)
 
-          default_function = if attgenerated.present?
-                               default
-                             else
-                               extract_default_function(default_value, default)
-                             end
+          if attgenerated.present?
+            default_function = default
+          else
+            default_function = extract_default_function(default_value, default)
+          end
 
           if (match = default_function&.match(/\Anextval\('"?(?<sequence_name>.+_(?<suffix>seq\d*))"?'::regclass\)\z/))
             serial = sequence_name_from_parts(table_name, column_name, match[:suffix]) == match[:sequence_name]
