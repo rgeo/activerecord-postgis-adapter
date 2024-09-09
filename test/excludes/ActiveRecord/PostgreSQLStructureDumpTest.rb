0064eeae00953ba06d0695e251bf85e3bed7793e
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2024-09-09 09:16:43 +0200

    more excludes

--- test/excludes/ActiveRecord/PostgreSQLStructureDumpTest.rb
+++ test/excludes/ActiveRecord/PostgreSQLStructureDumpTest.rb
@@ -9,3 +9,5 @@ teardown do
 	ActiveRecord::SchemaDumper.ignore_tables = @ignore_tables
 	ARTest.config["connections"].delete("postgresql")
 end
+
+exclude :test_structure_dump, TRIAGE_MSG + " / Likely due to CI installation of pg_dump vs postges"
