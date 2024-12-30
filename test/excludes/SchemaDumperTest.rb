038c0bfb98fafff6314f145f8d26f453f76791eb
Rafael Santos <formigarafa@gmail.com>
2024-12-30 21:14:04 +1300

    skip false failure
    schema dump is working fine.
    postgis just adds another foreign key entry in the dump within the boundary active records spec uses
    in the regex to test their case but the actual test would still be valid.
    added entry:
    ```
    add_foreign_key "layer", "topology", name: "layer_topology_id_fkey"
    ```

--- test/excludes/SchemaDumperTest.rb
+++ test/excludes/SchemaDumperTest.rb
@@ -3,3 +3,4 @@ exclude "test_schema_dump_when_changing_datetime_type_for_an_existing_app", TRIA
 if ActiveRecord::Base.lease_connection.pool.server_version(ActiveRecord::Base.lease_connection) < 15_00_00
   exclude "test_schema_dumps_unique_constraints", TRIAGE_MSG
 end
+exclude "test_foreign_keys_are_dumped_at_the_bottom_to_circumvent_dependency_issues", TRIAGE_MSG
